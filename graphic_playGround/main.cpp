f70ffdba4ba02fce26f317da00c0a63d038c7f85
CharlesHoo <Charles@cha.local>
2019-02-21 23:35:51 -0500

    Initial Commit

---
+++ graphic_playGround/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  graphic_playGround
+//
+//  Created by Charles on 2019-02-21.
+//  Copyright © 2019 Mu He. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
